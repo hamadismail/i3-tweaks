223a883c6fce91d29c4c869fa4fc40a18168f09f
Hamad Ismail <hamad.ismail.gub@gmail.com>
2025-04-02 16:50:12 +0600

    update-config

--- .config/polybar/scripts/powermenu.sh
+++ .config/polybar/scripts/powermenu.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-## Author  : Aditya Shakya
-## Mail    : adi1090x@gmail.com
-## Github  : @adi1090x
-## Twitter : @adi1090x
+## Author  :
+## Mail    :
+## Github  :
+## Twitter :
 
-dir="~/.config/polybar/cuts/scripts/rofi"
+dir="~/.config/polybar/scripts/rofi"
 uptime=$(uptime -p | sed -e 's/up //g')
 
 rofi_command="rofi -no-config -theme $dir/powermenu.rasi"
